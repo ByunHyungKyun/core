3cb983970548c75a601e8a6cb33fdd7591f15992
byun <princebyun@gmail.com>
2024-01-24 14:57:02 +0900

    김영한 spring 핵심원리 기본편

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -6,6 +6,7 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
@@ -14,7 +15,7 @@ public class ConfigurationSingletonTest {
 
 
     @Test
-    void configutarionTest(){
+    void configutarionTest() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
         MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
@@ -37,7 +38,15 @@ public class ConfigurationSingletonTest {
     }
 
 
+    @Test
+    void configurationDepp() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+
+    }
 
 
 }
