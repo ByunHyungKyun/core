8715af7bd0061e724f6d4f7d75928f15549f4abc
byun <princebyun@gmail.com>
2024-01-29 16:57:59 +0900

    김영한 spring 핵심원리 기본편

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,49 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+
+
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+//        prototypeBean1.destroy(); 이렇게 직접 명령을 해줘야 빈이 삭제가 된다.
+//        prototypeBean2.destroy();
+
+        ac.close();//프로토타입이라 close가 실행이 안된다.
+
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean{
+        @PostConstruct
+        public void init(){
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public  void destroy(){
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+
+}
